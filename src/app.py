f3009c2208c6ade3678587a80d896dff5a97bb65
jwild3 <74415994+jwild3@users.noreply.github.com>
2024-06-06 11:30:29 +0200

    feat: added first rag mvp

--- src/app.py
+++ src/app.py
@@ -1,122 +1,55 @@
-from flask import Flask, request
-from langchain_community.llms import Ollama
-from langchain_community.vectorstores import Chroma
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
-from langchain_community.document_loaders import PDFPlumberLoader
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
-from langchain.prompts import PromptTemplate
+import os
+from dotenv import load_dotenv
+from llama_index import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.vector_stores import ElasticsearchVectorStore
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.extractors import TitleExtractor, KeywordExtractor
+from llama_index.core.ingestion import IngestionPipeline
 
-app = Flask(__name__)
+# Load environment variables
+load_dotenv()
 
-folder_path = "db"
+# Initialize the document reader
+filename_fn = lambda filename: {"file_name": filename}
+documents = SimpleDirectoryReader("../data/pdf", file_metadata=filename_fn).load_data()
 
-cached_llm = Ollama(model="llama3")
+# Testing if the parsing of the docs was a success
+print(documents[0])
+len(documents)
 
-embedding = FastEmbedEmbeddings()
+# Initialize the embedding model
+embed_model = OpenAIEmbedding(model='text-embedding-3-large', embed_batch_size=100)
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1024, chunk_overlap=80, length_function=len, is_separator_regex=False
-)
+# Define metadata extractors
+transformations = [
+    SentenceSplitter(),
+    #TitleExtractor(nodes=5),
+    #KeywordExtractor(keywords=10),
+    #OpenAIEmbedding(model='text-embedding-3-large', embed_batch_size=100)
+]
 
-raw_prompt = PromptTemplate.from_template(
-    """ 
-    <s>[INST] You are a technical assistant good at searching docuemnts. If you do not have an answer from the provided information say so. [/INST] </s>
-    [INST] {input}
-           Context: {context}
-           Answer:
-    [/INST]
-"""
-)
+# Create ingestion pipeline
+pipeline = IngestionPipeline(transformations=transformations)
 
-
-@app.route("/ai", methods=["POST"])
-def aiPost():
-    print("Post /ai called")
-    json_content = request.json
-    query = json_content.get("query")
-
-    print(f"query: {query}")
-
-    response = cached_llm.invoke(query)
-
-    print(response)
-
-    response_answer = {"answer": response}
-    return response_answer
-
-
-@app.route("/ask_pdf", methods=["POST"])
-def askPDFPost():
-    print("Post /ask_pdf called")
-    json_content = request.json
-    query = json_content.get("query")
-
-    print(f"query: {query}")
-
-    print("Loading vector store")
-    vector_store = Chroma(persist_directory=folder_path, embedding_function=embedding)
-
-    print("Creating chain")
-    retriever = vector_store.as_retriever(
-        search_type="similarity_score_threshold",
-        search_kwargs={
-            "k": 20,
-            "score_threshold": 0.1,
-        },
+nodes = pipeline.run(
+    documents=documents,
+    show_progress=True
     )
 
-    document_chain = create_stuff_documents_chain(cached_llm, raw_prompt)
-    chain = create_retrieval_chain(retriever, document_chain)
+print(nodes[0].metadata)
 
-    result = chain.invoke({"input": query})
+# Connect to the ElasticSearchStore(Since there are still some issues with the Hydac in terms
+# of access to their Elastic SearchStore, we have to set it up on-premise in a docker container)
+vector_store = ElasticsearchStore(
+    es_url="http://localhost:9200",  
+    index_name="RAG-Testing",
 
-    print(result)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+index = VectorStoreIndex.from_documents(
+    documents, storage_context=storage_context
 
-    sources = []
-    for doc in result["context"]:
-        sources.append(
-            {"source": doc.metadata["source"], "page_content": doc.page_content}
-        )
-
-    response_answer = {"answer": result["answer"], "sources": sources}
-    return response_answer
-
-
-@app.route("/pdf", methods=["POST"])
-def pdfPost():
-    file = request.files["file"]
-    file_name = file.filename
-    save_file = "pdf/" + file_name
-    file.save(save_file)
-    print(f"filename: {file_name}")
-
-    loader = PDFPlumberLoader(save_file)
-    docs = loader.load_and_split()
-    print(f"docs len={len(docs)}")
-
-    chunks = text_splitter.split_documents(docs)
-    print(f"chunks len={len(chunks)}")
-
-    vector_store = Chroma.from_documents(
-        documents=chunks, embedding=embedding, persist_directory=folder_path
-    )
-
-    vector_store.persist()
-
-    response = {
-        "status": "Successfully Uploaded",
-        "filename": file_name,
-        "doc_len": len(docs),
-        "chunks": len(chunks),
-    }
-    return response
-
-
-def start_app():
-    app.run(host="0.0.0.0", port=8080, debug=True)
-
-
-if __name__ == "__main__":
-    start_app()
+# Query for testing
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do growing up?")
+print(response)
