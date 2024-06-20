34b4e9c244fa28e96835c1056d2eca0ba3d65ee9
Tobias Schneider <s8tsschn@uni-saarland.de>
2024-06-20 15:41:01 +0200

    add ollama and chromadb

--- src/app.py
+++ src/app.py
@@ -1,16 +1,32 @@
 import os
+import chromadb
 from dotenv import load_dotenv
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, ServiceContext
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.vector_stores.elasticsearch import ElasticsearchStore
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.extractors import TitleExtractor, KeywordExtractor
 from llama_index.core.ingestion import IngestionPipeline
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.embeddings.instructor import InstructorEmbedding
+from llama_index.vector_stores.chroma import ChromaVectorStore
+
+from llama_index.llms.ollama import Ollama
+from llama_index.core import Settings
+
+
 
 # The OPENAI should be in the env variables
 print("OPENAI_API_KEY:", os.environ['OPENAI_API_KEY'])
 # Load environment variables
 load_dotenv()
+
+# init chroma database
+chroma_client = chromadb.HttpClient()
+chroma_collection = chroma_client.get_or_create_collection(name="DocuRAG")
+
+# init llm
+Settings.llm = Ollama(model="llama2", request_timeout=60.0)
 
 # Initialize the document reader
 filename_fn = lambda filename: {"file_name": filename}
@@ -21,13 +37,18 @@ print(documents[0])
 len(documents)
 
 # Initialize the embedding model
-embed_model = OpenAIEmbedding(model='text-embedding-3-large', embed_batch_size=100)
+# openAI Embeddings
+# embed_model = OpenAIEmbedding(model='text-embedding-3-large', embed_batch_size=100)
+# HuggingFace Embedding
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+# Instructor Embedding
+# embed_model = InstructorEmbedding(model_name="hkunlp/instructor-base")
 
 # Define metadata extractors
 transformations = [
     SentenceSplitter(),
-    # TitleExtractor(nodes=5),
-    # KeywordExtractor(keywords=10),
+    TitleExtractor(nodes=5),
+    KeywordExtractor(keywords=10),
     # OpenAIEmbedding(model='text-embedding-3-large', embed_batch_size=100)
 ]
 
@@ -43,12 +64,15 @@ print(nodes[0].metadata)
 
 # Connect to the ElasticSearchStore(Since there are still some issues with the Hydac in terms
 # of access to their Elastic SearchStore, we have to set it up on-premise in a docker container)
-es = ElasticsearchStore(es_url="http://localhost:9200",
-                        index_name="RAG-Testing")
-storage_context = StorageContext.from_defaults(vector_store=es)
+# vector_store = ElasticsearchStore(es_url="http://localhost:9200", index_name="RAG-Testing")
+
+vector_store = ChromaVectorStore(chroma_collection= chroma_collection)
+
+
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
 index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
 
 # Query for testing
 query_engine = index.as_query_engine()
-response = query_engine.query("What did the author do growing up?")
+response = query_engine.query("How to init and get data from analog inputs on TTC 500 in C ?")
 print(response)
